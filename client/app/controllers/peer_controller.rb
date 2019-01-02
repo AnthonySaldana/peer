923e6398dce49ada85a891b4c2c50aa4c88afdf2
Anthony Saldana <anthonywebsol@gmail.com>
2019-01-02 12:48:40 -0800

    Tying up client call

--- client/app/controllers/peer_controller.rb
+++ client/app/controllers/peer_controller.rb
@@ -2,8 +2,14 @@ require 'httparty'
 
 class PeerController < ApplicationController
   def index
-    url = 'http://127.0.0.1:5000/peer?zip=90201'
-    response = HTTParty.get(url)
-    render json: response
+    zip = params[:zip].to_i
+    if zip != 0
+      url = 'http://127.0.0.1:5000/peer?zip=' + zip.to_s
+      response = HTTParty.get(url)
+      return render json: response
+    end
+    render default
+  end
+  def default
   end
 end
