1e8a06c4d0cda7ea7101fc8ca658e644719f6326
Anthony Saldana <anthonywebsol@gmail.com>
2019-01-02 13:09:27 -0800

    Mmoving client out

--- client/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  get "/peer", to: "peer#default"
-  get "/peer/*zip", to: "peer#index"
-  get 'welcome/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
